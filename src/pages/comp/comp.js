9f386c352f7afd27406089a5015d7630d6036adf
Songkeys <zy22132@nottingham.edu.cn>
2018-08-01 02:38:06 +0800

    ud

--- src/pages/comp/comp.js
+++ src/pages/comp/comp.js
@@ -1,21 +1,16 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import Btn from '../btn/btn'
 
 export default class Comp extends Component {
-  // defaultProps doesn't matter
-  // static defaultProps = {
-  //   isLoggedIn: false,
-  // }
+  static defaultProps = {
+    list: [],
+  }
 
   render() {
     return (
       <View>
-        {/* test passed with "isFinished": */}
-        #1: {this.props.isLoggedIn && <Btn />}
-
-        {/* test failed with "isFinished": */}
-        #2: {!this.props.isLoggedIn && <Btn />}
+        #1: {this.props.list.map(li => <View key={li}>{li}</View>)}
+        #2: {this.props.list.map(li => <View key={`${li}`}>{li}</View>)}
       </View>
     )
   }
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,33 +1,22 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import Comp from '../comp/comp'
-import noPropsComp from '../noPropsComp/noPropsComp'
 
 export default class Index extends Component {
   state = {
-    isFinished: true,
-    isFinished2: false,
-    isLoggedIn: true,
+    list: [],
+  }
+
+  componentDidMount() {
+    this.setState({
+      list: [1,2,3],
+    })
   }
 
   render() {
     return (
       <View>
-      {/* tests for "Cannot read property 'props' of undefined" */}
-      {/* test passed: */}
-      <Comp isLoggedIn={this.state.isLoggedIn} />
-
-      {/* test failed: */}
-      {this.state.isFinished && <Comp isLoggedIn={this.state.isLoggedIn} />}
-
-      {/* test passed: */}
-      {!this.state.isFinished2 && <Comp isLoggedIn={this.state.isLoggedIn} />}
-
-      {/* test failed: */}
-      {!!this.state.isFinished && <Comp isLoggedIn={this.state.isLoggedIn} />}
-
-      {/* tests for "all default props are null" */}
-      <noPropsComp />
+        <Comp list={this.state.list} />
       </View>
     )
   }
